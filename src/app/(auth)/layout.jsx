57e287c9d946e1330bd2b62eca7770882a04126d
Camila Arias <ariasruicamilaa>
2024-02-16 00:25:21 -0500

    organizaciòn carpetas

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,10 @@
+export default function authlayout({ children }) {
+    return (
+      <div >
+        <div>
+            Aqui va estar mi menu
+        </div>
+        <div>{children}</div>
+      </div>
+    );
+  }
---
+++ src/app/(auth)/pedidos/page.jsx
@@ -0,0 +1,9 @@
+const pedidosPage = () => {
+  return (
+    <div>
+      <h1>Pedidos</h1>
+    </div>
+  );
+}
+
+export default pedidosPage;
