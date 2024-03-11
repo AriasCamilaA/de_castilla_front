cd293c2d7f03c6485f9799618d77e3c2d244941f
Camila Arias <ariasruicamilaa>
2024-03-11 13:21:00 -0500

    Alerta insumos y productos

--- src/app/(pages)/(auth)/admin/components/generales/NavBar/NavBar.jsx
+++ src/app/(pages)/(auth)/admin/components/generales/NavBar/NavBar.jsx
@@ -4,19 +4,46 @@ import "./NavBar.css";
 import validateAccessToken from "app/utilities/auth/validateAccessToken";
 import { useState, useEffect } from "react";
 import cerrarSession from "app/utilities/auth/cerrarSession";
+import inventarioService from "app/services/inventario/Inventario_service";
 
 const NavBar = () => {
+
   const [user, setUser] = useState(null);
+  const [lowStockItems, setLowStockItems] = useState([]);
 
   useEffect(() => {
     validateAccessToken()
       .then((user) => {
         setUser(user);
+        // Llamar a la función para verificar el stock bajo
+        checkLowStock();
+        // Establecer una validación del inventario cada 30 segundos
+        const interval = setInterval(checkLowStock, 10000); // 30 segundos
+        return () => clearInterval(interval); // Limpiar el intervalo en caso de que el componente se desmonte
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
+
+
+  // Función para verificar el stock bajo
+  const checkLowStock = async () => {
+    try {
+      inventarioService.getInventario()
+        .then(
+          (response) => {
+            const inventario = response;
+            const lowStock = inventario.filter(item => {
+              return item.stock_inventario <= 5; // Filtrar los elementos con stock menor a 5
+            });
+            setLowStockItems(lowStock);
+          }
+        )
+    } catch (error) {
+      console.error("Error al obtener el inventario:", error);
+    }
+  };
 
   const handleLogout = async () => {
     try {
@@ -82,15 +109,40 @@ const NavBar = () => {
                         <img className="icon" src='/assets/icons/LogoCasa.png'/>
                     </Link>
                 }
-                
+                {
+                    lowStockItems.length > 0 && (
+                    <div className="dropdown me-5">
+                        <button className="icon dropdown-toggle menu-user text-dark" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <p>Alertas</p>
+                        {lowStockItems.length > 0 && (
+                        <div className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">{lowStockItems.length}</div>)
+                        }
+                        </button>
+                        <ul className="dropdown-menu user-dropdown">
+                        <li>  
+                            {/* Mostrar notificación si hay elementos con stock bajo */}
+                            {lowStockItems.length > 0 && (
+                                <div className="notification-badge">
+                                {lowStockItems.map(item => (
+                                    <p key={item.id_inventario} className="text-light m-2 border-bottom border-white">
+                                    {item.insumo ? item.insumo.nombre_insumo : item.producto.nombre_producto}: {item.stock_inventario}
+                                    </p>
+                                ))}
+                                </div>
+                            )}
+                        </li>
+                        </ul>
+                    </div>
+                    )
+                }
             </div>
             <div className="menu-logo">
                 <img src='/assets/img/logoClaro.png'/>
-            </div>
+            </div>   
             <div className="dropdown">
                 <button className="dropdown-toggle menu-user" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                <img className="icon" src='/assets/icons/Logo Usuario.png'/>
-                {user && `${user.nombre_usuario} ${user.apellido_usuario}`}
+                    <img className="icon" src='/assets/icons/Logo Usuario.png'/>
+                    {user && (`${user.nombre_usuario} ${user.apellido_usuario}`)}
                 </button>
                 <ul className="dropdown-menu user-dropdown">
                 <li>
@@ -113,7 +165,7 @@ const NavBar = () => {
                 </li>
                 </ul>
             </div>
-            </nav>
+        </nav>
 
     );
 }
