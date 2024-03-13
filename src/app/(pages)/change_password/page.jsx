d161a5222a8ae621ece932c1f23dd697c1483ec8
Camila Arias <ariasruicamilaa>
2024-03-13 14:09:28 -0500

    Restablecer contraseña

--- src/app/(pages)/change_password/page.jsx
+++ src/app/(pages)/change_password/page.jsx
@@ -1,37 +1,82 @@
 "use client"
-import React, { useState } from "react";
-import passwordResetService from "app/services/password_service";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from 'next/navigation'
+import usuariosService from "app/services/usuarios/usuarios_service";
+import { showAlert } from "app/utilities";
+import "app/css/generales/style.css";
+import "app/css/generales/forms.css";
+import "app/css/generales/login.css";
+import "app/css/generales/botones.css";
+
+
 
 const ResetPasswordConfirmation = () => {
+  const [successMessage, setSuccessMessage] = useState(null);
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState(null);
+  const [user, setUser] = useState({});
+  const [idUser, setIdUser] = useState(null);
+  const searchParams = useSearchParams()
+
+  const getId = () => {
+    setIdUser(searchParams.get('user'))
+  }
+  
+
+  useEffect(() => {
+    getId()
+  }, []);
+
+  useEffect(() => {
+    if(idUser){
+      try {
+        usuariosService.getUsuariosById(idUser)
+        .then((response) => {
+          setUser(response);
+        }).catch((error) => {
+          showAlert("error", "Información no encontrada", `El usuario identificado con ${idUser} no existe`)
+        })  
+      } catch (error) {
+        showAlert("error", "Error al cargar el usuario", "No existe")
+      }
+    }
+    
+  }, [idUser]);
 
   const handlePasswordChange = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
-      setError("Las contraseñas no coinciden.");
-      return;
-    }
-    try {
-      // Cambiar la contraseña
-      const response = await passwordResetService.resetPassword(email, password);
-      setSuccessMessage("Tu contraseña ha sido restablecida con éxito.");
-    } catch (error) {
-      console.error("Error al restablecer la contraseña:", error);
-      setError("Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde.");
+      showAlert("error", "Error", "Las contraseñas no coinciden");
+    }else{
+      try {
+        usuariosService.updateUsuarios({...user, password: password})
+          .then((response) => {
+            showAlert("success","","Contraseña restablecida con éxito.");
+            setPassword("");
+            setConfirmPassword("");
+          }).catch((error) => {
+            console.error("Error al restablecer la contraseña:", error);
+            setError("Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde.");
+          })
+          
+      } catch (error) {
+        console.error("Error al restablecer la contraseña:", error);
+        setError("Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde.");
+      }
     }
   };
 
   return (
     <div className="fondoTranslucido">
-      <section className="container">
+      <section className="container h-50">
         <form className="form" onSubmit={handlePasswordChange}>
           <h2 className="title">Restablecer contraseña</h2>
-          <div className="form__inputs">
-            <div className="divLogin">
-              <div className="inputConLogo">
+            <div className="d-flex flex-column">
+              <div className="inputConLogo my-2 ">
+                <i className="form__icon">
+                  <img src="assets/icons/Key.png" alt="Logo Llave" />
+                </i>
                 <input
                   className="form__input"
                   type="password"
@@ -44,7 +89,10 @@ const ResetPasswordConfirmation = () => {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <div className="inputConLogo">
+              <div className="inputConLogo my-2 ">
+                <i className="form__icon">
+                  <img src="assets/icons/Key.png" alt="Logo Llave" />
+                </i>
                 <input
                   className="form__input"
                   type="password"
@@ -57,13 +105,10 @@ const ResetPasswordConfirmation = () => {
                   onChange={(e) => setConfirmPassword(e.target.value)}
                 />
               </div>
-              {error && <span className="input-message-error text-danger">{error}</span>}
-              {successMessage && <span className="input-message-success text-success">{successMessage}</span>}
             </div>
-            <button className="btn btn_largo" type="submit">
+            <button className="btn btn_oscuro" type="submit">
               Restablecer contraseña
             </button>
-          </div>
         </form>
       </section>
     </div>
