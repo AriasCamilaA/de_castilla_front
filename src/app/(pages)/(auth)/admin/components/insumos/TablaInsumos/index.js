eb153e6097a8c985959ebd5e890cae2af4ace3a4
Camila Arias <ariasruicamilaa>
2024-03-18 00:18:29 -0500

    Fix error proveedores actualizar

--- src/app/(pages)/(auth)/admin/components/insumos/TablaInsumos/index.js
+++ src/app/(pages)/(auth)/admin/components/insumos/TablaInsumos/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActualizarInsumo from '../ActualizarInsumo';
 import insumosService from 'app/services/inventario/insumos_service';
 import withReactContent from 'sweetalert2-react-content';
@@ -6,33 +6,34 @@ import Swal from 'sweetalert2';
 import { showAlert } from 'app/utilities';
 
 const TablaInsumos = ({ insumos, filtroNombre, actualizarListaInsumos }) => {
-    const insumosFiltrados = insumos.filter((insumo) =>
-    insumo.nombre_insumo.toLowerCase().includes(filtroNombre.toLowerCase())
-  );
+    const [insumo, setInsumo] = useState({});
+    const insumosFiltrados = insumos.filter((item) =>
+        item.nombre_insumo.toLowerCase().includes(filtroNombre.toLowerCase())
+    );
 
-  const handlrerEliminarInsumo = (insumo) => {
+    const handlerEliminarInsumo = (insumo) => {
         insumo.estado = 0;
         insumosService.updateInsumo(insumo)
-        .then(() => {
-            actualizarListaInsumos(); // Actualiza la lista global de insumos
-            showAlert('success', 'Insumo Eliminado', 'El insumo ha sido eliminado exitosamente');
-        })
-        .catch(() => {
-            showAlert('error', 'Error', 'No se pudo eliminar el insumo');
-        });
-    };
-  const handlrerActivarInsumo = (insumo) => {
-        insumo.estado = 1;
-        insumosService.updateInsumo(insumo)
-        .then(() => {
-            actualizarListaInsumos(); // Actualiza la lista global de insumos
-            showAlert('success', 'Insumo Activado', '');
-        })
-        .catch(() => {
-            showAlert('error', 'Error', 'No se pudo activar el insumo');
-        });
+            .then(() => {
+                actualizarListaInsumos(); // Actualiza la lista global de insumos
+                showAlert('success', 'Insumo Eliminado', 'El insumo ha sido eliminado exitosamente');
+            })
+            .catch(() => {
+                showAlert('error', 'Error', 'No se pudo eliminar el insumo');
+            });
     };
 
+    const handlerActivarInsumo = (insumo) => {
+        insumo.estado = 1;
+        insumosService.updateInsumo(insumo)
+            .then(() => {
+                actualizarListaInsumos(); // Actualiza la lista global de insumos
+                showAlert('success', 'Insumo Activado', '');
+            })
+            .catch(() => {
+                showAlert('error', 'Error', 'No se pudo activar el insumo');
+            });
+    };
 
     const eliminarInsumo = (insumo) => {
         const MySwal = withReactContent(Swal);
@@ -45,10 +46,11 @@ const TablaInsumos = ({ insumos, filtroNombre, actualizarListaInsumos }) => {
             confirmButtonColor: '#732f48',
         }).then((result) => {
             if (result.isConfirmed) {
-                handlrerEliminarInsumo(insumo);
+                handlerEliminarInsumo(insumo);
             }
         });
-    }
+    };
+
     const activarInsumo = (insumo) => {
         const MySwal = withReactContent(Swal);
         MySwal.fire({
@@ -60,53 +62,56 @@ const TablaInsumos = ({ insumos, filtroNombre, actualizarListaInsumos }) => {
             confirmButtonColor: '#732f48',
         }).then((result) => {
             if (result.isConfirmed) {
-                handlrerActivarInsumo(insumo);
+                handlerActivarInsumo(insumo);
             }
         });
-    }
+    };
 
-  return (
-    <div className="tabla container mt-4">
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Estado</th>
-                        <th className="tabla__opcion" scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {insumosFiltrados
-                .sort((a, b) => b.id_insumo - a.id_insumo)
-                .map((insumo) => (
-                    <tr key={insumo.id_insumo}>
-                    <td>{insumo.id_insumo}</td>
-                    <td>{insumo.nombre_insumo}</td>
-                    <td>{insumo.estado == 1 ? (
-                            <div className="cursor-pointer" onClick={()=>{eliminarInsumo(insumo)}}>
-                            ✅
-                            </div>
-                            ) : (
-                            <div className="cursor-pointer" onClick={()=>{activarInsumo(insumo)}}>
-                            ❌
-                            </div>
-                        )}
-                    </td>
-                    <td className="tabla__opcion">
-                        <div className="opciones_tabla">
-                        <div className="cursor-pointer" data-bs-toggle="modal" data-bs-target="#actualizarInsumo">
-                            🔍
-                        </div>
-                        <ActualizarInsumo actualizarListaInsumos={actualizarListaInsumos} insumo={insumo}/>
-                        </div>
-                    </td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </div>
-  );
+    return (
+        <>
+            <div className="tabla container mt-4">
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nombre</th>
+                            <th scope="col">Estado</th>
+                            <th className="tabla__opcion" scope="col">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {insumosFiltrados
+                            .sort((a, b) => b.id_insumo - a.id_insumo)
+                            .map((insumo) => (
+                                <tr key={insumo.id_insumo}>
+                                    <td>{insumo.id_insumo}</td>
+                                    <td>{insumo.nombre_insumo}</td>
+                                    <td>
+                                        {insumo.estado === 1 ? (
+                                            <div className="cursor-pointer" onClick={() => { eliminarInsumo(insumo) }}>
+                                                ✅
+                                            </div>
+                                        ) : (
+                                            <div className="cursor-pointer" onClick={() => { activarInsumo(insumo) }}>
+                                                ❌
+                                            </div>
+                                        )}
+                                    </td>
+                                    <td className="tabla__opcion">
+                                        <div className="opciones_tabla">
+                                            <div className="cursor-pointer" data-bs-toggle="modal" data-bs-target="#actualizarInsumo" onClick={() => setInsumo(insumo)}>
+                                                🔍
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </table>
+            </div>
+            <ActualizarInsumo actualizarListaInsumos={actualizarListaInsumos} insumo={insumo} />
+        </>
+    );
 };
 
 export default TablaInsumos;
