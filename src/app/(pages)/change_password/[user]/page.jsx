a6466f2e5d107fe8dcaa5f0d1aa06368d2c21afe
Camila Arias <ariasruicamilaa>
2024-03-18 21:35:53 -0500

    Validaciones y redireccionamiento cambiar contraseña

--- src/app/(pages)/change_password/[user]/page.jsx
+++ src/app/(pages)/change_password/[user]/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router"; // Importar el hook useRouter
 import usuariosService from "app/services/usuarios/usuarios_service";
 import { showAlert } from "app/utilities";
 import "app/css/generales/style.css";
@@ -7,36 +8,31 @@ import "app/css/generales/forms.css";
 import "app/css/general/login.css";
 import "app/css/generales/botones.css";
 import Link from "next/link";
+import { redirect } from "next/dist/server/api-utils";
 
-
-
-const ResetPasswordConfirmation = ({params}) => {
-  const [successMessage, setSuccessMessage] = useState(null);
+const ResetPasswordConfirmation = ({ params }) => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState(null);
   const [user, setUser] = useState({});
   const [email, setEmail] = useState(params.user);
 
-
   useEffect(() => {
-    if(email){
+    if (email) {
       try {
-        usuariosService.getUsuariosByEmail(email)
-        .then((response) => {
-          if(response[0].length === 0){
+        usuariosService.getUsuariosByEmail(email).then((response) => {
+          if (response[0].length === 0) {
             throw new Error("Usuario no encontrado");
-          }else{
+          } else {
             setUser(response[0]);
           }
         }).catch((error) => {
-          showAlert("error", "Información no encontrada", `El usuario identificado con ${email} no existe`)
-        })  
+          showAlert("error", "Información no encontrada", `El usuario identificado con ${email} no existe`);
+        });
       } catch (error) {
-        showAlert("error", "Error al cargar el usuario", "No existe")
+        showAlert("error", "Error al cargar el usuario", "No existe");
       }
     }
-    
   }, [email]);
 
   const handlePasswordChange = async (e) => {
@@ -47,24 +43,50 @@ const ResetPasswordConfirmation = ({params}) => {
       return;
     }
 
+    // Validar longitud mínima de contraseña
+    if (password.length < 8) {
+      showAlert("error", "Error", "La contraseña debe tener al menos 8 caracteres.");
+      return;
+    }
+
+    // Validar que las contraseñas sean iguales
     if (password !== confirmPassword) {
-      showAlert("error", "Error", "Las contraseñas no coinciden");
-    }else{
-      try {
-        usuariosService.updateUsuarios({...user, password: password})
-          .then((response) => {
-            showAlert("success","","Contraseña restablecida con éxito.");
-            setPassword("");
-            setConfirmPassword("");
-          }).catch((error) => {
-            console.error("Error al restablecer la contraseña:", error);
-            setError("Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde.");
-          })
-          
-      } catch (error) {
-        console.error("Error al restablecer la contraseña:", error);
-        setError("Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde.");
-      }
+      showAlert("error", "Error", "Las contraseñas no coinciden.");
+      return;
+    }
+
+    // Validar que la contraseña contenga al menos un número
+    if (!/\d/.test(password)) {
+      showAlert("error", "Error", "La contraseña debe contener al menos un número.");
+      return;
+    }
+
+    try {
+      usuariosService
+        .updateUsuarios({ ...user, password: password })
+        .then((response) => {
+          showAlert("success", "", "Contraseña restablecida con éxito.");
+          setPassword("");
+          setConfirmPassword("");
+          window.location.href = "/login";
+        })
+        .catch((error) => {
+          console.error(
+            "Error al restablecer la contraseña:",
+            error
+          );
+          setError(
+            "Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde."
+          );
+        });
+    } catch (error) {
+      console.error(
+        "Error al restablecer la contraseña:",
+        error
+      );
+      setError(
+        "Error al restablecer la contraseña. Por favor, inténtalo de nuevo más tarde."
+      );
     }
   };
 
@@ -73,46 +95,46 @@ const ResetPasswordConfirmation = ({params}) => {
       <section className="container_login container_restablecer">
         <form className="form" onSubmit={handlePasswordChange}>
           <h2 className="title">Restablecer contraseña</h2>
-            <div className="d-flex flex-column">
-              <div className="inputConLogo my-2 ">
-                <i className="form__icon">
-                  <img src="/assets/icons/Key.png" alt="Logo Llave" />
-                </i>
-                <input
-                  className="form__input"
-                  type="password"
-                  name="password"
-                  placeholder="Nueva contraseña"
-                  id="password"
-                  required
-                  autoComplete="none"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="inputConLogo my-2 ">
-                <i className="form__icon">
-                  <img src="/assets/icons/Key.png" alt="Logo Llave" />
-                </i>
-                <input
-                  className="form__input"
-                  type="password"
-                  name="confirmPassword"
-                  placeholder="Confirmar contraseña"
-                  id="confirmPassword"
-                  required
-                  autoComplete="none"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </div>
+          <div className="d-flex flex-column">
+            <div className="inputConLogo my-2 ">
+              <i className="form__icon">
+                <img src="/assets/icons/Key.png" alt="Logo Llave" />
+              </i>
+              <input
+                className="form__input"
+                type="password"
+                name="password"
+                placeholder="Nueva contraseña"
+                id="password"
+                required
+                autoComplete="none"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <button className="btn btn_oscuro" type="submit">
-              Restablecer contraseña
-            </button>
-            <Link href='/login' className="border-top-oscuro pt-2">
-              ¿Iniciar Sesión?
-            </Link>
+            <div className="inputConLogo my-2 ">
+              <i className="form__icon">
+                <img src="/assets/icons/Key.png" alt="Logo Llave" />
+              </i>
+              <input
+                className="form__input"
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirmar contraseña"
+                id="confirmPassword"
+                required
+                autoComplete="none"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+          </div>
+          <button className="btn btn_oscuro" type="submit">
+            Restablecer contraseña
+          </button>
+          <Link href="/login" className="border-top-oscuro pt-2 mt-4">
+            ¿Iniciar Sesión?
+          </Link>
         </form>
       </section>
     </div>
